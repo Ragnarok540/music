3ca407074ff5c495830c73f84398c8916d392f2d
Edgar Nova <ragnarok540@gmail.com>
2025-03-19 11:40:12 -0500

    adsr envelope

--- src/sound_synthesis.py
+++ src/sound_synthesis.py
@@ -2,11 +2,28 @@ import numpy as np
 import sounddevice as sd
 
 
+def apply_envelope(sound: np.ndarray,
+                   adsr: dict,
+                   sample_rate: int = 44100) -> np.ndarray:
+    sound = sound.copy()
+    a_s = int(adsr['attack'] * sample_rate)
+    d_s = int(adsr['decay'] * sample_rate)
+    r_s = int(adsr['release'] * sample_rate)
+    s_s = len(sound) - (a_s + d_s + r_s)
+
+    sound[:a_s] *= np.linspace(0, 1, a_s)
+    sound[a_s:a_s + d_s] *= np.linspace(1, adsr['sustain'], d_s)
+    sound[a_s + d_s:a_s + d_s + s_s] *= adsr['sustain']
+    sound[a_s + d_s + s_s:] *= np.linspace(adsr['sustain'], 0, r_s)
+
+    return sound
+
+
 def white_noise(duration: float = 1.0,
                 amplitude: float = 0.5,
                 sample_rate: int = 44100) -> np.ndarray:
-    n_samples = int(duration * sample_rate)
-    noise = np.random.uniform(-1, 1, n_samples)
+    n_s = int(duration * sample_rate)
+    noise = np.random.uniform(-1, 1, n_s)
     noise *= amplitude
 
     return noise
@@ -16,8 +33,8 @@ def sine_tone(frequency: int = 440,
               duration: float = 1.0,
               amplitude: float = 0.5,
               sample_rate: int = 44100) -> np.ndarray:
-    n_samples = int(duration * sample_rate)
-    time_points = np.linspace(0, duration, n_samples, False)
+    n_s = int(duration * sample_rate)
+    time_points = np.linspace(0, duration, n_s, False)
     sine = np.sin(2 * np.pi * frequency * time_points)
     sine *= amplitude
 
@@ -26,19 +43,28 @@ def sine_tone(frequency: int = 440,
 
 if __name__ == '__main__':
     # my_sound = white_noise()
-    # my_sound = sine_tone()
+    # my_sound = sine_tone(duration=3.0)
 
     # sine_1 = sine_tone(frequency=200, amplitude=0.6)
     # sine_2 = sine_tone(frequency=400, amplitude=0.3)
     # sine_3 = sine_tone(frequency=800, amplitude=0.2)
     # my_sound = sum([sine_1, sine_2, sine_3])
 
-    # sines = [sine_tone(frequency=200 * i + 50, amplitude=0.7/i) for i in range(1, 31, 2)]
+    # sines = [sine_tone(frequency=200 * i + 50, duration=3.0, amplitude=0.7/i) for i in range(1, 31, 2)]
     # my_sound = sum(sines)
 
     sines = [sine_tone(frequency=200, duration=3.0, amplitude=0.6),
              sine_tone(frequency=205, duration=3.0, amplitude=0.6)]
     my_sound = sum(sines)
+
+    adsr = {
+        'attack': 0.5,
+        'decay': 0.2,
+        'sustain': 0.6,
+        'release': 0.5,
+    }
+
+    my_sound = apply_envelope(my_sound, adsr)
 
     sd.play(my_sound)
     sd.wait()
